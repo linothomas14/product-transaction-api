c47f662d8e2fa2d93a76253bac570bdfbafa92ee
Yulyano Thomas Djaya <ydjaya@pajak.io>
2022-12-27 06:52:14 +0700

    fix: token issuer

--- middleware/jwt-auth.go
+++ middleware/jwt-auth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -14,16 +15,27 @@ import (
 func AuthorizeJWT(jwtService service.AuthService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.BuildResponse("Use Bearer token please", nil)
+			c.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+
 		if authHeader == "" {
 			response := helper.BuildResponse("No token found", nil)
 			c.AbortWithStatusJSON(http.StatusBadRequest, response)
 			return
 		}
+
+		authHeader = strings.Replace(authHeader, "Bearer ", "", -1)
+
 		token, err := jwtService.ValidateToken(authHeader)
+
 		if token.Valid {
 			claims := token.Claims.(jwt.MapClaims)
 			log.Println("Claim[user_id]: ", claims["user_id"])
-			log.Println("Claim[issuer] :", claims["issuer"])
+			log.Println("Claim[issuer] :", claims["iss"])
 		} else {
 			log.Println(err)
 			log.Println(token)
